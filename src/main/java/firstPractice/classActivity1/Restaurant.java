56732036037a18684d1502769fa375006337d2c4
ShalomVerlaUCLL <Shalomleinyuy.verla@student.ucll.be>
2025-03-20 22:49:12 +0100

    first commit

---
+++ src/main/java/firstPractice/classActivity1/Restaurant.java
@@ -0,0 +1,16 @@
+package firstPractice.classActivity1;
+
+public class Restaurant {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private int age;
+
+
+}
+
+
